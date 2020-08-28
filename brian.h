92105c19bda31901c321740061de24aa8dc9dee7
Brian Pereira <pereirabrian@gmail.com>
2020-08-28 15:10:49 +0530

    Fixed errors in the functions in brian.h

---
+++ brian.h
@@ -0,0 +1,18 @@
+/* 
+   Author: Brian Pereira
+   Dated:  30 August 2019
+   Purpose: A library of functions.
+   Last update: 28th August 2020
+*/
+
+
+void mean() {}
+void median() {}
+void mode() {}
+
+void swap(int x, int y)
+{
+}
+
+void sort_asc() {}
+void sort_desc() {}
